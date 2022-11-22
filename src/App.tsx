781ed5a0888d5a108550a8fd25dc08d6b95cc27d
Anny88 <any.klim@mail.ru>
2022-11-22 22:39:21 +0100

    filter func implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useMemo, useState } from 'react'
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import { AutocompleteInput, OfferCardList } from "./components";
 import { useFetchPriceOffers } from "./hooks";
-import { COUNTRY_CODES } from "./utils";
+import {COUNTRY_CODES, filterPriceOffers} from "./utils";
+import './App.css'
 
 function App() {
   const { data: response, loading } = useFetchPriceOffers();
   const [origin, setOrigin] = useState('');
   const [destination, setDestination] = useState('');
-  const [filteredItems, setFilteredItems] = useState(response);
 
-  console.log(response);
-  const handleOriginChange = (val: string) => {
-      setOrigin(val);
-      setFilteredItems(response?.filter((item) => (item.origin === val || !val) && (item.destination === destination || !destination)));
-  }
-
-  const handleDestinationChange = (val: string) => {
-      setDestination(val);
-      setFilteredItems(response?.filter((item) => (item.origin === origin || !origin) && (item.destination === val || !val)));
-  }
+  const filteredItems = useMemo(() =>
+      filterPriceOffers(response, origin, destination),
+      [response, origin, destination]);
 
   return (
     <div className="App">
@@ -30,16 +22,16 @@ function App() {
             <Box mb={6} mt={6} width="100%" >
                 <Grid container spacing={2} justifyContent="center">
                     <Grid item xs={12} md={6}>
-                        <AutocompleteInput id="origin" label="origin" options={COUNTRY_CODES} value={origin} onChange={handleOriginChange} />
+                        <AutocompleteInput id="origin" label="origin" options={COUNTRY_CODES} value={origin} onChange={setOrigin} />
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <AutocompleteInput id="destination" label="destination" options={COUNTRY_CODES} value={destination} onChange={handleDestinationChange} />
+                        <AutocompleteInput id="destination" label="destination" options={COUNTRY_CODES} value={destination} onChange={setDestination} />
                     </Grid>
                 </Grid>
             </Box>
 
             <Box mb={6} width="100%">
-                <OfferCardList loading={loading} items={response} />
+                <OfferCardList loading={loading} items={filteredItems} />
             </Box>
         </Container>
     </div>
--- src/utils/filterPriceOffers.ts
+++ src/utils/filterPriceOffers.ts
@@ -1,7 +1,10 @@
 import { PriceOffer } from "../models";
 
-export const filterPriceOffers = (priceOffers: PriceOffer[] | undefined, origin: string, destination: string) => {
+export const filterPriceOffers = (priceOffers: PriceOffer[] | undefined, origin: string, destination: string)
+    : PriceOffer[] | undefined => {
     if ((!origin && !destination) || !priceOffers) {
         return priceOffers;
     }
+    return priceOffers.filter((item) =>
+        (!origin || item.origin === origin) && (!destination || item.destination === destination));
 }
