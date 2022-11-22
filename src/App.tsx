68a4a6747c465150275616a511ed896ab06d56c0
Anny88 <any.klim@mail.ru>
2022-11-22 21:36:48 +0100

    added country codes

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,24 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import { AutocompleteInput, OfferCardList } from "./components";
 import { PriceOffer } from "./models";
-
-const inputOptions = ['ALA', 'DUS', 'FRA', 'ISS', 'DUB'];
+import { COUNTRY_CODES } from "./utils";
 
 const initialResponse: PriceOffer[] = [];
-
+function onlyUnique(value:any, index: any, self:any) {
+    return self.indexOf(value) === index;
+}
 function App() {
   const [origin, setOrigin] = useState('');
   const [destination, setDestination] = useState('');
   const [response, setResponse] = useState(initialResponse);
   const [filteredItems, setFilteredItems] = useState(response);
   const [loading, setLoading] = useState(true);
+
+  const or = response.map((item) => item.origin);
+  const des = response.map((item) => item.origin);
+
+
+  console.log([...or, ...des].filter(onlyUnique).sort());
 
   useEffect(() => {
       fetch("http://localhost:8080/promotions/priceoffers/ond", {
@@ -48,10 +55,10 @@ function App() {
             <Box mb={6} mt={6} width="100%" >
                 <Grid container spacing={2} justifyContent="center">
                     <Grid item xs={12} md={6}>
-                        <AutocompleteInput id="origin" label="origin" options={inputOptions} value={origin} onChange={handleOriginChange} />
+                        <AutocompleteInput id="origin" label="origin" options={COUNTRY_CODES} value={origin} onChange={handleOriginChange} />
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <AutocompleteInput id="destination" label="destination" options={inputOptions} value={destination} onChange={handleDestinationChange} />
+                        <AutocompleteInput id="destination" label="destination" options={COUNTRY_CODES} value={destination} onChange={handleDestinationChange} />
                     </Grid>
                 </Grid>
             </Box>
---
+++ src/utils/countryCodes.ts
@@ -0,0 +1,7 @@
+// TODO: move to backend
+export const COUNTRY_CODES = ['AFN', 'AGJ', 'ALA', 'ALQ', 'ANH', 'ANP', 'APF', 'ASS', 'AVB', 'AXC', 'BHB', 'BQS', 'CGN','CFG',
+    'CMW', 'CMZ', 'CNM', 'CPA', 'CTU', 'CZN', 'DJN', 'DUS', 'FRA', 'GBI', 'GIC', 'GNZ', 'GPN', 'GWD', 'HAT', 'HCR', 'HDR',
+    'HLN', 'IAS', 'IFO', 'IWO', 'JIW', 'KBE', 'KDH', 'KED', 'KKK', 'LBF', 'LIR', 'LTO', 'MAE', 'MHI', 'MKA', 'MOG',
+    'MSA', 'MVX', 'MZY', 'NDG', 'NPL', 'NUP', 'NYM', 'OSZ', 'PBC', 'PKR', 'PRY', 'QEF', 'QLP', 'RAI', 'RNB', 'RPA', 'RTS',
+    'SDJ', 'SNX', 'SOX', 'SPB', 'SWU', 'SZS', 'TAC', 'TBV', 'TDX', 'TLE', 'TLM', 'TMS', 'TND', 'TPJ', 'TRS', 'TTN', 'TWU',
+    'UMS', 'USH', 'VEL', 'VQQ', 'VRA', 'WWR', 'YAY', 'YFH', 'YGV', 'YHH', 'YHK', 'YNY', 'YPO', 'YQW', 'YUB', 'ZNC', 'ZQW'];
